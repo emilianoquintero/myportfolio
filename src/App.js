cd0d1aa057528e44b9fa98638f5046b29cc0c81e
Emiliano Quintero <qb.emiliano@gmail.com>
2025-02-17 10:26:18 -0700

    Inicio

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='App-container'>
+        <header className='App-header'>
+          <a>
+            <button> Menu nav</button>
+          </a>
+          <div className='App-list-menu'>
+            <ul>
+              <li>About</li>
+              <li>Resume</li>
+              <li>Works</li>
+              <li>Apps</li>
+              <li>Contact</li>
+            </ul>
+          </div>
+        </header>
+        <div className='App-Profile-card'>
+          <div className='Profile-card-align'>
+            <div className='Profile-photo'></div>
+            <div className='Profile-name'> Emiliano Quintero</div>
+            <div className='Profile-title'>Web Developer</div>
+            <div className='Profile-social'>
+              <a>GitHub</a>
+              <a>Linkln</a>
+              <a>Spotify</a>
+            </div>
+            <div className='Profile-card-bottoms'>
+              <a>Downald CV</a>
+              <a>Contact Me</a>
+            </div>
+          </div>
+        </div>
+        <div className='Card-inner Animated active FadeInLeft'>
+        <div className='Card-inner-content-about'>
+            <div className='Title'>
+              <span className='First-word'>About </span>
+                Me
+            </div>
+            <div className='row'>
+              <div className='col'>
+                <div class="text-box">
+                  <p>Contenido del párrafo...</p>
+                </div>
+              </div>
+              <div className='col'>
+                <div class="text-box">
+                  <p>Contenido del párrafo...</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className='Card-inner Animated hidden FadeOutLeft'>
+          <div className='Card-inner-content'>
+            <h1>Adios</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
